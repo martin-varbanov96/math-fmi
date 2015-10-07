dc3043f349a2a848dfe508c68f79baa4a946e9cb
martin_varbanov96 <martin_varbanov96@abv.bg>
2015-10-07 23:13:20 +0300

    Binomial theorem BETA
    added a solution to the Binomial theorem

---
+++ binom/app.js
@@ -0,0 +1,30 @@
+﻿var sum = 0;
+var globalResult = 0;
+
+main();
+
+function main(){
+    console.log(calcBinom(2, 3, 6, 6, 0));
+}
+ 
+function calcBinom(a, b, nconst, n, k){    
+    if (n < 0) {
+        return;
+    }
+    getComb(nconst, k, 1)
+    sum += Math.pow(a, n) * Math.pow(b, k) * globalResult;
+    calcBinom(a, b, nconst, n - 1, k + 1);
+    return sum;
+}
+
+function getComb(n, k, resultComb){
+    if (k == 0) {
+        globalResult = resultComb;
+    }
+    if (k > 0) {       
+        resultComb *= n / k;
+        getComb(n - 1, k - 1, resultComb);
+        return resultComb;
+    }
+    return resultComb;
+}
