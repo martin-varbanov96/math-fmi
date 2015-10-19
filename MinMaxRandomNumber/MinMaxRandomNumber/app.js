494290f0b0096bbdc174616d2763cdfffb85badf
martin_varbanov96 <martin_varbanov96@abv.bg>
2015-10-19 17:28:57 +0300

    Random number generator
    Added a random number generator in a given range

---
+++ MinMaxRandomNumber/MinMaxRandomNumber/app.js
@@ -0,0 +1,8 @@
+﻿for (var i = 0; i <= 10000; i++) {
+    var a = getRandomInRange(0, 100);
+    console.log(a);
+}
+
+function getRandomInRange(min, max){ 
+    return Math.floor(Math.random()*(max-min)+min);
+}
