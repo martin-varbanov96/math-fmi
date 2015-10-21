6b31da1ab846ff3c3773012e78210e6c35c046c6
martin_varbanov96 <martin_varbanov96@abv.bg>
2015-10-21 20:13:39 +0300

    UI
    Added UI but still the there are problems with the output

--- isABiggerThanB/temp/app.js
+++
@@ -1,8 +0,0 @@
-﻿var start = new Date().getTime();
-var end;
-for(var i=0;i<=50;i++){
-    console.log(i);
-}
-end = new Date().getTime();
-var final = end - start;
-console.log(final);
---
+++ isABiggerThanB/temp/js/app.js
@@ -0,0 +1,31 @@
+﻿$(function () {
+    var $a;
+    var $b;
+
+    $('button').click(function () {
+        $a = $('#a').val();
+        $b = $('#b').val();
+        
+       var $big = getBigger($b, $a);
+       var $small = getSmaller($b, $a);
+        
+        $('#bigger').text($big);
+        $('#smaller').text($small);
+    });
+    
+});
+
+
+//a simple function for getting a bigger element
+function getBigger(a, b) {
+    return a ^ (a ^ b) & -(a < b);
+}
+
+//a simple function for getting a smaller element
+function getSmaller(a, b) {
+    return (a < b) ? a : b;
+}
+
+
+
+
