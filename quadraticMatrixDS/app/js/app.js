7736262a706b53080a9295c8874db36be2c72203
martin_varbanov96 <martin_varbanov96@abv.bg>
2015-10-23 00:54:17 +0300

    BETA
    Added a basic jQuery project which can determine the DS of a 2x2 Matrix

---
+++ quadraticMatrixDS/app/js/app.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    $('#submit').click(function () {
+        
+        var $topLeft = $("#a11").val();
+        var $topRight = $("#a12").val();
+        var $botLeft = $("#a21").val();
+        var $botRight = $("#a22").val();
+        
+        var $result = getQuadratticDS($topLeft, $topRight, $botLeft, $botRight);
+        
+        $("#result").text($result);
+    });
+});
+
+
+function getQuadratticDS(a11, a12, a21, a22){
+    return a11 * a22 - a12 * a21;
+}
