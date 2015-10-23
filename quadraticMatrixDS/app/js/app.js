ebff464c478b1f64b4db539a34b71cb7447579a1
martin_varbanov96 <martin_varbanov96@abv.bg>
2015-10-23 18:38:46 +0300

    UI 1.1
    added some UI changes

--- quadraticMatrixDS/app/js/app.js
+++ quadraticMatrixDS/app/js/app.js
@@ -1,18 +1,23 @@
 ﻿$(document).ready(function () {
-    $('#submit').click(function () {
-        
-        var $topLeft = $("#a11").val();
-        var $topRight = $("#a12").val();
-        var $botLeft = $("#a21").val();
-        var $botRight = $("#a22").val();
-        
-        var $result = getQuadratticDS($topLeft, $topRight, $botLeft, $botRight);
-        
-        $("#result").text($result);
-    });
+    $('#submit').click(main);
+    $(document).keypress(function (e) {
+        if (e.which == 13) {
+            main();
+        }
+    });        
 });
 
-//generate a fun   
+//generate a DS of the matrix   
 function getQuadratticDS(a11, a12, a21, a22){
     return a11 * a22 - a12 * a21;
+}
+function main() {    
+    var $topLeft = $("#a11").val();
+    var $topRight = $("#a12").val();
+    var $botLeft = $("#a21").val();
+    var $botRight = $("#a22").val();
+    
+    var $result = getQuadratticDS($topLeft, $topRight, $botLeft, $botRight);
+    
+    $("#result").text($result);
 }
