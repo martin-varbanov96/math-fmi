bd8eba842d9dd3f33440b9bbc162489677b84ab1
martin_varbanov96 <martin_varbanov96@abv.bg>
2015-12-15 23:17:31 +0200

    1.0
    IT WORKS !!

--- matrixDS/script/app.js
+++ matrixDS/script/app.js
@@ -2,74 +2,77 @@
 
 main();
 
-function main(){
+function main() {
     var matrix = [
-        [1, 2, 3, 4],
-        [5, 6, 7, 8],
-        [9, 10, 11, 12],
-        [13, 14, 15, 16]
+        [-6, -5, 0, 0, 0, 0],
+        [-1, -6, -5, 0, 0, 0],
+        [0, -1, -6, -5, 0, 0],
+        [0, 0, -1, -6, -5, 0],
+        [0, 0, 0, -1, -6, -5],
+        [0, 0, 0, 0, -1, -6]
     ];
     var a = getDS(matrix);
     console.log(a);
 
 }
-function get3x3DS(inputMatrix){
+function get3x3DS(inputMatrix) {
     return (inputMatrix[0][0] * inputMatrix[1][1] * inputMatrix[2][2]) + (inputMatrix[0][1] * inputMatrix[1][2] * inputMatrix[2][0]) + (inputMatrix[0][2] * inputMatrix[1][0] * inputMatrix[2][1]) - (inputMatrix[0][2] * inputMatrix[1][1] * inputMatrix[2][0]) - (inputMatrix[0][0] * inputMatrix[1][2] * inputMatrix[2][1]) - (inputMatrix[0][1] * inputMatrix[1][0] * inputMatrix[2][2]);
 }
 
-function get2x2DS(inputMatrix){
+function get2x2DS(inputMatrix) {
     return (inputMatrix[0][0] * inputMatrix[1][1]) - (inputMatrix[0][1] * inputMatrix[1][0]);
 }
 
-function is3x3(inputMatrix){
-    if(inputMatrix.length == 3){
+function is3x3(inputMatrix) {
+    if (inputMatrix.length == 3) {
         return true;
     }
     return false;
 }
-function isMatrixSquare(inputMatrix){
-    for(var i = 0; i < inputMatrix.length; i++){
-        if(inputMatrix.length != inputMatrix[i].length){
+function isMatrixSquare(inputMatrix) {
+    for (var i = 0; i < inputMatrix.length; i++) {
+        if (inputMatrix.length != inputMatrix[i].length) {
             return false;
         }
     }
     return true;
 }
-function getDS(inputMatrix){
-    if(isMatrixSquare(inputMatrix) == false){        
-       return NaN; 
+function getDS(inputMatrix) {
+    if (isMatrixSquare(inputMatrix) == false) {
+        return NaN;
     }
-    if(is3x3(inputMatrix) == true){
+    if (is3x3(inputMatrix) == true) {
         return get3x3DS(inputMatrix);
     }
     var outputMatrix = getEmptyMatrix([], inputMatrix.length - 1);
     var index = -1;
     var jIndex = 0;
     var kIndex = 0;
-    for (var i = 0; i < inputMatrix.length; i++){
+    var temp = 0;
+    for (var i = 0; i < inputMatrix.length; i++) {
         index *= -1;
-        for (var j=1; j < inputMatrix.length; j++){
-            for (var k=0; k < inputMatrix.length; k++){
-                if(i != k){
+        for (var j = 1; j < inputMatrix.length; j++) {
+            for (var k = 0; k < inputMatrix.length; k++) {
+                if (i != k) {
                     outputMatrix[jIndex][kIndex] = inputMatrix[j][k];
-                    console.log(outputMatrix); // TEMP!!!
+                    kIndex++;
                 }
-                kIndex++;
-
             }
             kIndex = 0;
             jIndex++;
-
         }
+        jIndex = 0;
+        console.log(outputMatrix); // TEMP!!!
         console.log("Getting deeper");//TEMP!!
-        outputMatrix = getDS(outputMatrix);
-        return inputMatrix[0][i] * index * outputMatrix;
-    }    
+        temp += inputMatrix[0][i] * index * getDS(outputMatrix);
+    }
+    return temp;
+
 }
 
 //Returns an empty Matrix nxn
-function getEmptyMatrix(inputMatrix, n){
-    for(var i=0; i<n; i++) {
+function getEmptyMatrix(inputMatrix, n) {
+    for (var i = 0; i < n; i++) {
         inputMatrix[i] = new Array(n);
     }
     return inputMatrix;
